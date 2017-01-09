3594d6fa7eb8d0f746c39372776258c244c0b3cd
OLLIS-LABTOP\okram <o.kramm@hotmail.de>
2017-01-09 17:30:04 +0100

    addet UE4 project

---
+++ ZombieProject/Source/ZombieProject/ZombieProjectGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "ZombieProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIEPROJECT_API AZombieProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
