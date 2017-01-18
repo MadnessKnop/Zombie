98e2eb3a3c9e691d0b1ed21afec166b1f67c0e91
OLLIS-LABTOP\okram <o.kramm@hotmail.de>
2017-01-18 16:09:03 +0100

    Base Controll for the player

--- ZombieProject/Source/ZombieProject/RandomPlayer_BP.cpp
+++ ZombieProject/Source/ZombieProject/RandomPlayer_BP.cpp
@@ -7,6 +7,9 @@
 // Sets default values
 ARandomPlayer_BP::ARandomPlayer_BP()
 {
+	BaseTurnRate = 45.f;
+	BaseLookUpRate = 45.f;
+
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
@@ -29,10 +32,17 @@ void ARandomPlayer_BP::Tick( float DeltaTime )
 // Called to bind functionality to input
 void ARandomPlayer_BP::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
+	check(PlayerInputComponent);
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
 	//MovementInput
 	InputComponent->BindAxis("MoveForward", this, &ARandomPlayer_BP::MoveForward);
 	InputComponent->BindAxis("MoveRight", this, &ARandomPlayer_BP::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
 void ARandomPlayer_BP::MoveForward(float amount)
@@ -49,5 +59,15 @@ void ARandomPlayer_BP::MoveRight(float amount)
 	{
 		AddMovementInput(GetActorRightVector(), amount);
 	}
+}
+
+void ARandomPlayer_BP::TurnAtRate(float Rate)
+{
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ARandomPlayer_BP::LookUpAtRate(float Rate)
+{
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
 }
 
--- ZombieProject/Source/ZombieProject/RandomPlayer_BP.h
+++ ZombieProject/Source/ZombieProject/RandomPlayer_BP.h
@@ -10,12 +10,21 @@ class ZOMBIEPROJECT_API ARandomPlayer_BP : public ACharacter
 {
 	GENERATED_BODY()
 
+		//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		//class UCameraComponent* FirstPersonCameraComponent;
+
 public:
 	// Sets default values for this character's properties
 	ARandomPlayer_BP();
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		float BaseTurnRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		float BaseLookUpRate;
 	
 	// Called every frame
 	virtual void Tick( float DeltaSeconds ) override;
@@ -23,8 +32,14 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+
 	void MoveForward(float amount);
 
 	void MoveRight(float amount);
+
+	void TurnAtRate(float Rate);
+
+	void LookUpAtRate(float Rate);
 	
 };
